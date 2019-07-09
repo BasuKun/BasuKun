797e02b0006bb62270f1b0fdb66be125a3586190
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-07-09 13:14:16 -0400

    lol

---
+++ Projects/New Unity Project/Assets/Scripts/BalloonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour
+{
+    //how much it grows each time
+    [Range(0, 10)]
+    public float scaleFactor = 1.2f;
+
+    //maximum scale
+    public float maxScale = 3;
+
+    void OnValidate()
+    {
+        if(scaleFactor <= 1)
+        {
+            print("Scale Factor is too small");
+        }
+    }
+    //Method that will detect mouse press
+    void OnMouseDown()
+    {
+        //Access the scale & increase the scale
+        //transform.localScale = transform.localScale * scaleFactor
+        transform.localScale *= scaleFactor;
+
+        //check if we've passed the maxScale
+        if(transform.localScale.x >= maxScale)
+        {
+            //if so, destroy the balloon
+            Destroy(gameObject);
+        }
+    }
+}
