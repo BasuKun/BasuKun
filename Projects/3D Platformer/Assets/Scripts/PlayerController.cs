e8f25f4418ec7bd0046c68ffd0bad8f965bbbe95
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-07-16 13:31:20 -0400

    jump and camera

--- Projects/3D Platformer/Assets/Scripts/PlayerController.cs
+++ Projects/3D Platformer/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
 
     private Rigidbody rig;
 
@@ -17,6 +18,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            TryJump();
+        }
     }
 
     void Move ()
@@ -25,9 +31,41 @@ public class PlayerController : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
 
+        // calculate the direction we need to move in
         Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
         dir.y = rig.velocity.y;
 
+        // set the rigidbody velocity
         rig.velocity = dir;
+
+        Vector3 facingDir = new Vector3(xInput, 0, zInput);
+
+        if(facingDir.magnitude > 0)
+        {
+            transform.forward = facingDir;
+        }
+    }
+
+    // called when we press the "jump" button
+    void TryJump ()
+    {
+        // create a ray pointing downwards from each corner of the player
+        Ray ray1 = new Ray(transform.position + new Vector3(0.5f, 0, 0.5f), Vector3.down);
+        Ray ray2 = new Ray(transform.position + new Vector3(-0.5f, 0, 0.5f), Vector3.down);
+        Ray ray3 = new Ray(transform.position + new Vector3(0.5f, 0, -0.5f), Vector3.down);
+        Ray ray4 = new Ray(transform.position + new Vector3(-0.5f, 0, -0.5f), Vector3.down);
+
+        // check if each ray hits something
+        bool cast1 = Physics.Raycast(ray1, 0.7f);
+        bool cast2 = Physics.Raycast(ray2, 0.7f);
+        bool cast3 = Physics.Raycast(ray3, 0.7f);
+        bool cast4 = Physics.Raycast(ray4, 0.7f);
+
+        // shoot the raycast
+        if (cast1 || cast2 || cast3 || cast4)
+        {
+            // add force upwards
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
