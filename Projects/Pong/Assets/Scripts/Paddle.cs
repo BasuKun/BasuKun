aa88df08ac165a2797fdc07141f514b6589b2edc
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-07-25 13:02:50 -0400

    PONG

---
+++ Projects/Pong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 1f;
+
+    void Update()
+    {
+        float verticalMovement = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, verticalMovement * speed);
+    }
+}
