fc517c9e60e7d5164a6ad31bec083eb2a458c8b3
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-08-05 13:12:35 -0400

    Space Attackers started

---
+++ Projects/Space Attackers/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public float horizontalLimit = 2.5f;
+    public GameObject missilePrefab;
+    public float firingSpeed = 3f;
+    public float missileLastingTime = 2f;
+
+    private bool fired = false;
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * speed, 0);
+
+        if (transform.position.x > horizontalLimit)
+        {
+            transform.position = new Vector2(horizontalLimit, transform.position.y);
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
+        if (transform.position.x < -horizontalLimit)
+        {
+            transform.position = new Vector2(-horizontalLimit, transform.position.y);
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+
+        if (Input.GetAxis("Fire1") == 1f)
+        {
+            if (fired == false)
+            {
+                fired = true;
+
+                GameObject missileInstance = Instantiate(missilePrefab);
+                missileInstance.transform.SetParent(transform.parent);
+                missileInstance.transform.position = transform.position;
+                missileInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(0, firingSpeed);
+
+                Destroy(missileInstance, missileLastingTime);
+            }
+        }
+        else
+        {
+            fired = false;
+        }
+    }
+}
