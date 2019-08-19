47fb12b0820e824ac89e7fb9c9cbc2a387e358b2
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-08-19 14:36:05 -0400

    FPS Project

---
+++ Projects/FPS/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    [Header("Camera")]
+    public float lookSensitivity;
+    public float maxLookX;
+    public float minLookX;
+    private float rotX;
+
+    private Camera cam;
+    private Rigidbody rig;
+
+    void Awake()
+    {
+        // get the components
+        cam = Camera.main;
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
