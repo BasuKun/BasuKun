6ef49e7f674cd689e5d6bb4ebb73130c2247c7dd
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-08-20 15:03:07 -0400

    more fps

--- Projects/FPS/Assets/Scripts/Player.cs
+++ Projects/FPS/Assets/Scripts/Player.cs
@@ -16,16 +16,68 @@ public class Player : MonoBehaviour
 
     private Camera cam;
     private Rigidbody rig;
+    private Weapon weapon;
 
     void Awake()
     {
         // get the components
         cam = Camera.main;
         rig = GetComponent<Rigidbody>();
+        weapon = GetComponent<Weapon>();
+
+        // disable cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        
+        Move();
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            TryJump();
+        }
+
+        if(Input.GetButton("Fire1"))
+        {
+            if (weapon.CanShoot())
+            {
+                weapon.Shoot();
+            }
+        }
+
+        CamLook();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+    }
+
+    void CamLook()
+    {
+        float y = Input.GetAxis("Mouse X") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+
+        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
+    }
+
+    void TryJump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, 1.1f))
+        {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
---
+++ Projects/FPS/Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform muzzle;
+
+    public int curAmmo;
+    public int maxAmmo;
+    public bool infiniteAmmo;
+
+    public float bulletSpeed;
+
+    public float shootRate;
+    private float lastShootTime;
+    private bool isPlayer;
+
+    void Awake()
+    {
+        if (GetComponent<Player>())
+        {
+            isPlayer = true;
+        }
+    }
+
+    // can we shoot a bullet
+    public bool CanShoot()
+    {
+        if(Time.time - lastShootTime >= shootRate)
+        {
+            if (curAmmo > 0 || infiniteAmmo == true)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Shoot()
+    {
+        lastShootTime = Time.time;
+        curAmmo--;
+
+        GameObject bullet = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
+
+        // set the velocity
+        bullet.GetComponent<Rigidbody>().velocity = muzzle.forward * bulletSpeed;
+    }
+}
