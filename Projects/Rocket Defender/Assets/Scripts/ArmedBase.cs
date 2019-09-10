e4b0aa2f683129a81dbe6383ea8c886def65ed52
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-09-10 13:21:31 -0400

    stuff

---
+++ Projects/Rocket Defender/Assets/Scripts/ArmedBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmedBase : Base
+{
+    public float shootSpeed = 5f;
+
+    [SerializeField]
+    private Rocket rocket;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Rocket rocketInstance = Instantiate(rocket);
+        rocketInstance.Launch(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position, shootSpeed);
+    }
+}
