35fa9e55b09d870abc0a9f2fd75d1e1bd084d00f
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-10-01 15:18:58 -0400

    new project

---
+++ Courses Files/Toads and Fireflies - Source Files/Assets/Scripts/Game.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game : MonoBehaviour
+{
+    public int[] playerScores;
+    [SerializeField]
+    private float gameTime;
+    [SerializeField]
+    private TextMeshProUGUI[] playerScoresText;
+    [SerializeField]
+    private TextMeshProUGUI timerText, playerWinnerText;
+    [SerializeField]
+    private GameObject gameOverPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScores = new int[2];
+    }
+
+    public void AddPoints(int player, int amount)
+    {
+        playerScores[player] += amount;
+        UpdateUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTime -= Time.deltaTime;
+        // update timer text
+        timerText.text = gameTime.ToString("00.0");
+        if (gameTime <= 0)
+        {
+            // game over!
+            Time.timeScale = 0;
+            int winner = playerScores[0] > playerScores[1] ? 1 : 2;
+            gameOverPanel.SetActive(true);
+            playerWinnerText.text = string.Format("Player {0} wins!", winner);
+        }
+    }
+
+    private void UpdateUI()
+    {
+        // update ui text
+        playerScoresText[0].text = playerScores[0].ToString();
+        playerScoresText[1].text = playerScores[1].ToString();
+    }
+}
---
+++ Courses Files/Toads and Fireflies - Source Files/Assets/Scripts/Toad.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toad : MonoBehaviour
+{
+    public int player;
+    [SerializeField]
+    private Transform[] jumpPathPoints;
+    [SerializeField]
+    private float jumpSpeed;
+    [SerializeField]
+    private Tongue tongue;
+    [SerializeField]
+    private Sprite[] toadSprites;
+    private bool grounded, reachedTargetPoint;
+    private int positionIndex;
+    private Vector2 jumpDirection;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        grounded = true;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // if grounded and pressed action button, jump
+        if (Input.GetButtonDown("Action" + player) && grounded)
+        {
+            Debug.Log("Jump!");
+            jumpDirection = positionIndex == 0 ? Vector2.right : Vector2.left;
+            spriteRenderer.sprite = toadSprites[1];
+            grounded = false;
+        }
+        // If not grounded and pressed action button, attack
+        else if (Input.GetButtonDown("Action" + player))
+        {
+            StartCoroutine(Attack());
+        }
+        Move();
+    }
+
+    IEnumerator Attack()
+    {
+        tongue.gameObject.SetActive(true);
+        yield return new WaitForSeconds(.7f);
+        tongue.gameObject.SetActive(false);
+    }
+
+    private void Move()
+    {
+        if (!grounded)
+        {
+            // If close enough to target point, return true
+            reachedTargetPoint = Vector2.Distance(transform.position, jumpPathPoints[positionIndex].position) <= .1f;
+            // What to do if jumping to the right
+            if (jumpDirection == Vector2.right)
+            {
+                if (reachedTargetPoint)
+                {
+                    positionIndex++;
+                }
+                if (ReachedLandingPad())
+                {
+                    positionIndex = 2;
+                }
+            }
+            // what to do if jumping to the left
+            else
+            {
+                if (reachedTargetPoint)
+                {
+                    positionIndex--;
+                }
+                if (ReachedLandingPad())
+                {
+                    positionIndex = 0;
+                }
+            }
+        }
+        // Move towards target path point
+        transform.position = Vector2.MoveTowards(transform.position, jumpPathPoints[positionIndex].position, jumpSpeed);
+    }
+
+    bool ReachedLandingPad()
+    {
+        if (jumpDirection == Vector2.right && positionIndex == jumpPathPoints.Length
+            || jumpDirection == Vector2.left && positionIndex == -1)
+        {
+            grounded = true;
+            transform.Rotate(Vector3.up, 180f);
+            spriteRenderer.sprite = toadSprites[0];
+            return true;
+        }
+        return false;
+    }
+}
