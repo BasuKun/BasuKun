da31c9b04da6d799e906ab23a826012ab1afed9a
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-10-04 13:03:21 -0400

    yo

---
+++ Projects/Toads and Fireflies/Assets/Scripts/Firefly.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firefly : MonoBehaviour
+{
+    public int pointValue;
+    public float moveSpeed;
+    public float sinSpeed;
+    public float sinLength;
+    private Vector2 direction;
+
+    private Vector3 newPosition;
+
+    private void Awake()
+    {
+        sinSpeed = Random.Range(0.5f, 2.5f);
+        sinLength = Random.Range(3, 5);
+    }
+
+    void Update()
+    {
+        newPosition = transform.position;
+        newPosition.y += Mathf.Sin(Time.time * sinLength) * sinSpeed * Time.deltaTime;
+        transform.position = newPosition;
+        Move();
+    }
+
+    public void Setup(float speed, Vector2 direction)
+    {
+        this.pointValue = (int)speed * 3;
+        this.moveSpeed = speed;
+        this.direction = direction;
+    }
+
+    private void Move()
+    {
+        transform.Translate((direction * 1.3f) * Time.deltaTime);
+    }
+}
---
+++ Projects/Toads and Fireflies/Assets/Scripts/FireflySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Firefly[] fireflies;
+
+    private float spawnDelay;
+
+    void Start()
+    {
+        spawnDelay = 1f;
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        Firefly flyInstance = Instantiate(fireflies[Random.Range(0, fireflies.Length)], new Vector3(transform.position.x, transform.position.y + Random.Range(-2, 2), transform.position.z), Quaternion.identity);
+        int power = Random.Range(2, 6);
+        flyInstance.Setup(power, transform.right);
+        yield return new WaitForSeconds(spawnDelay + Random.Range(1, 3));
+        StartCoroutine(Spawn());
+    }
+}
