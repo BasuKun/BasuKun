d8a3e46fcc39c7edae1fae0e35a316d25e5ce369
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-10-17 15:20:53 -0400

    stuff

---
+++ Projects/Mobile Course/Assets/Scripts/DragGesture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragGesture : MonoBehaviour
+{
+    private Camera cam;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            Vector3 lastPos = Input.touches[0].position - Input.touches[0].deltaPosition;
+            Vector3 curPos = Input.touches[0].position;
+
+            Vector3 dir = cam.ScreenToWorldPoint(lastPos) - cam.ScreenToWorldPoint(curPos);
+
+            transform.position += dir;
+        }
+    }
+}
---
+++ Projects/Mobile Course/Assets/Scripts/ZoomGesture.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomGesture : MonoBehaviour
+{
+    public float zoomRate = 0.015f;
+    public Vector2 minMaxZoom;
+
+    private Camera cam;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 2)
+        {
+            Vector3 aPrevPos = Input.touches[0].position - Input.touches[0].deltaPosition;
+            Vector3 bPrevPos = Input.touches[1].position - Input.touches[1].deltaPosition;
+
+            float prevMag = (aPrevPos - bPrevPos).magnitude;
+            float curMag = (Input.touches[0].position - Input.touches[1].position).magnitude;
+
+            float diff = curMag - prevMag;
+
+            if (cam.orthographic)
+            {
+                cam.orthographicSize -= diff * zoomRate;
+                cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minMaxZoom.x, minMaxZoom.y);
+            }
+            else
+            {
+                transform.position += transform.forward * (diff * zoomRate);
+            }
+        }
+    }
+}
