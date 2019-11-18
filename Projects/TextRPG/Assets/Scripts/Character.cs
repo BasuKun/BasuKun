2cdd8138337b8e0616fc6e951209064556e5a0d7
BasuKun <52501602+BasuKun@users.noreply.github.com>
2019-11-18 16:21:05 -0500

    new project

---
+++ Projects/TextRPG/Assets/Scripts/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int Energy { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int Gold { get; set; }
+    public Vector2 RoomIndex { get; set; }
+    public List<string> Inventory { get; set; }
+}
